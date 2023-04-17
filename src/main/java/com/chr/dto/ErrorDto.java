3a7c93afccbfbfd2fbd484487ff9bc344a418982
Christian Bautista <cristianmendoza009@hotmail.com>
2023-04-17 08:10:37 -0600

    Error dto

---
+++ src/main/java/com/chr/dto/ErrorDto.java
@@ -0,0 +1,13 @@
+package com.chr.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+public class ErrorDto {
+    @Getter
+    @Setter
+    public String message;
+
+}
